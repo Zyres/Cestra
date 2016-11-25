b72f7c4e1986476cc5d51789b438b856fbdaf781
Zyres <mr@eichbuehlerhof.de>
2016-11-25 21:36:21 +0100

    Added boost and mysql-connector to project

--- cmake/Platform/Windows.cmake
+++ cmake/Platform/Windows.cmake
@@ -1,3 +1,25 @@
+if(IS_64BIT)
+    set(MYSQL_LIBS ${CMAKE_SOURCE_DIR}/dep/mysql-connector/win64/lib/mysqlcppconn.lib CACHE INTERNAL "MySQL libs." FORCE)
+else()
+    set(MYSQL_LIBS ${CMAKE_SOURCE_DIR}/dep/mysql-connector/win32/lib/mysqlcppconn.lib CACHE INTERNAL "MySQL libs." FORCE)
+endif()
+
+set(BOOST_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/dep/boost CACHE INTERNAL "Boost dir." FORCE)
+
+if(IS_64BIT)
+    set(MYSQL_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/dep/mysql-connector/win64/include CACHE INTERNAL "MySQL dir." FORCE)
+else()
+    set(MYSQL_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/dep/mysql-connector/win32/include CACHE INTERNAL "MySQL dir." FORCE)
+endif()
+
+if(IS_64BIT)
+   set(DEP_DLLS ${CMAKE_SOURCE_DIR}/dep/mysql-connector/win64/lib)
+else()
+   set(DEP_DLLS ${CMAKE_SOURCE_DIR}/dep/mysql-connector/win32/lib)
+endif()
+
+set(INSTALL_DEPS ${DEP_DLLS}/mysqlcppconn.dll)
+
 if(MSVC)
     include(${CMAKE_SOURCE_DIR}/cmake/Compilers/msvc.cmake)
 else()
