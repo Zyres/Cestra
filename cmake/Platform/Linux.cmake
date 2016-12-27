f06deb529b70dcb4e04899184bd5d193959cd770
Zyres <mr@eichbuehlerhof.de>
2016-12-27 13:57:20 +0100

    Linked mysql lib for *nix

--- cmake/Platform/Linux.cmake
+++ cmake/Platform/Linux.cmake
@@ -1,3 +1,9 @@
+if(IS_64BIT)
+    set(MYSQL_LIBS ${CMAKE_SOURCE_DIR}/dep/lib/x64/libmysql.lib CACHE INTERNAL "MySQL libs." FORCE)
+else()
+    set(MYSQL_LIBS ${CMAKE_SOURCE_DIR}/dep/lib/x86/libmysql.lib CACHE INTERNAL "MySQL libs." FORCE)
+endif()
+
 set(MYSQL_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/dep/mysql CACHE INTERNAL "MySQL dir." FORCE)
 
 if(CMAKE_COMPILER_IS_GNUCXX)
