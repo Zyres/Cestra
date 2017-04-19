7a7d4850487291dc6a3c96ad9c62c80c9457d232
The-Broccoli <korepp.f@gmail.com>
2017-04-19 19:05:27 +0200

    Init

--- cmake/Compilers/msvc.cmake
+++
@@ -1,7 +0,0 @@
-if(MSVC_VERSION VERSION_LESS 19.0.24210) #2015 3
-    message(FATAL_ERROR "This Visual studio version is not supported")
-endif()
-
-message(STATUS "Applying settings for ${CMAKE_CXX_COMPILER_ID}")
-
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
--- cmake/Platform/Linux.cmake
+++
@@ -1,7 +0,0 @@
-find_package(MySQL REQUIRED)
-
-if(CMAKE_COMPILER_IS_GNUCXX)
-    include(${CMAKE_SOURCE_DIR}/cmake/Compilers/gcc.cmake)
-else()
-    message(FATAL_ERROR "Compiler not supported")
-endif()
